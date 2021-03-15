d5f47a54a92f498f90c385bbbf65029db2f4bc14
Gabriel Dias <60017886+gabrieldias102@users.noreply.github.com>
2021-03-14 21:15:33 -0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,18 +1,22 @@
-const nomeDoHeroi = document.getElementById("pesquisa").value; 
-const container = document.querySelector('#marvel-row');
-const chosenHero = nomeDoHeroi;
-let contentHTML = '';
+const chosenHero = document.getElementById("pesquisa").value;
+const container = document.querySelector("#marvel-row");
+let contentHTML = "";
 
-function functionByUser() {
+function heroByUser() {
   event.preventDefault();
-  const nomeDoHeroi = document.getElementById("pesquisa").value; 
-  showHero(nomeDoHeroi)
+  const chosenHero = document.getElementById("pesquisa").value;
+  showHero(chosenHero);
+  clearFields("pesquisa");
 }
 
-function showHero(chosenHero){
+function clearFields(textBox) {
+  document.getElementById(textBox).value = "";
+}
+
+function showHero(chosenHero) {
   const urlAPI = `https://gateway.marvel.com/v1/public/characters?name=${chosenHero}&ts=1&apikey=e19ceca6efc686a15dbe4fbc6533d381&hash=30d109de6ccc288ebb3a98896715e30e`;
   fetch(urlAPI)
-    .then(res => res.json())
+    .then((res) => res.json())
     .then((json) => {
       for (const hero of json.data.results) {
         let urlHero = hero.urls[0].url;
@@ -23,23 +27,21 @@ function showHero(chosenHero){
               </a>
               <div class="hero-text">
                 <h2 class="hero-title">${hero.name}</h2>
-                <p class = "hero-comic"> PRIMEIRA APARIÇÃO: </p>
+                <p class = "hero-comic"> PRIMEIRO QUADRINHO: </p>
                 <p class="comics"> ${hero.comics.items[0].name} <p/>
-                <p class = "hero-comic"> SEGUNDA APARIÇÃO: </p>
+                <p class = "hero-comic"> SEGUNDO QUADRINHO: </p>
                 <p class="comics"> ${hero.comics.items[1].name} <p/>
               </div>
           </div>`;
       }
       container.innerHTML = contentHTML;
-    })
+    });
 }
 
 const marvel = {
-    render(){
-    }
-  };
-  
-showHero('thor');
-showHero('hulk');
-marvel.render();
+  render() {},
+};
 
+showHero("thor");
+showHero("iron man");
+marvel.render();
